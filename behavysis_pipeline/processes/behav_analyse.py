e8bf97ea8956d89bc2b12f36e9f6f5290f851d98
tlee08 <timothy.e.lee@sydney.edu.au>
2024-07-01 17:12:28 +1000

    abcd

--- behavysis_pipeline/processes/behav_analyse.py
+++ behavysis_pipeline/processes/behav_analyse.py
@@ -50,45 +50,25 @@ class BehavAnalyse:
         # Calculating the deltas (changes in body position) between each frame for the subject
         configs = ExperimentConfigs.read_json(configs_fp)
         fps, _, _, _, bins_ls, cbins_ls = AnalyseMixin.get_configs(configs)
-        # models_ls = configs.user.classify_behaviours
         # Loading in dataframe
         behavs_df = BehavMixin.read_feather(behavs_fp)
         # Setting all na and -1 values to 0
         behavs_df = behavs_df.fillna(0).map(lambda x: np.maximum(0, x))
         # Getting the behaviour names and each user_behav for the behaviour
-        # Not incl. the `actual`, `pred`, or `prob` (`prob` shouldn't be here anyway) column
+        # Not incl. the `pred` or `prob` (`prob` shouldn't be here anyway) column
         columns = behavs_df.columns[
             np.isin(
                 behavs_df.columns.to_frame(index=False)[BehavCN.OUTCOMES.value],
-                DFIOMixin.enum_to_list(BehavColumns),
+                [BehavColumns.PROB.value, BehavColumns.PRED.value],
                 invert=True,
             )
         ]
         behavs_df = behavs_df[columns]
+        # Writing the behavs_df to the fbf file
         fbf_fp = os.path.join(out_dir, "fbf", f"{name}.feather")
         DFIOMixin.write_feather(behavs_df, fbf_fp)
-        # Getting the behav_outcomes dict from the configs file
-        # behav_outcomes = {
-        #     i: [
-        #         j
-        #         for j in behavs_df[i].columns.unique()
-        #         if np.isin(j, DFIOMixin.enum_to_list(BehavColumns))
-        #     ]
-        #     for i in behavs_df.columns.unique("behaviours")
-        # # }
-        # # Converting to the analysis dataframe format (with specific index and column levels)
-        # analysis_df = AnalyseMixin.init_df(behavs_df.index)
-        # # Keeping the `actual` and all user_behavs columns
-        # a = BehavColumns.ACTUAL.value
-        # for behav, user_behavs in behav_outcomes:
-        #     analysis_df[(behav, a)] = behavs_df[(behav, a)].values
-        #     for i in user_behavs:
-        #         analysis_df[(behav, i)] = behavs_df[(behav, i)].values
-        # # Saving analysis_df
-        # fbf_fp = os.path.join(out_dir, "fbf", f"{name}.feather")
-        # DFIOMixin.write_feather(analysis_df, fbf_fp)
-
         # Updating the column level names of behavs_df
+        # (summary_binned_behavs only works this way)
         behavs_df.columns.names = DFIOMixin.enum_to_list(AnalysisCN)
         # Making the summary and binned dataframes
         AggAnalyse.summary_binned_behavs(
