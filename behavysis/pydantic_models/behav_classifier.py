beb9ceb6d702e0f2664183588fce7e3fa8bb8cbc
tlee08 <timothyeelim.lee@gmail.com>
2025-01-31 03:43:24 +1100

    abcd

--- behavysis/pydantic_models/behav_classifier.py
+++ behavysis/pydantic_models/behav_classifier.py
@@ -6,11 +6,11 @@ class BehavClassifierConfigs(PydanticBaseModel):
     behav_name: str = "behav_name"
     seed: int = 42
     oversample_ratio: float = 0.1
-    undersample_ratio: float = 0.4
+    undersample_ratio: float = 0.5
 
     clf_struct: str = "clf"  # Classifier type (defined in ClfTemplates)
     pcutoff: float = 0.2
     test_split: float = 0.2
     val_split: float = 0.2
     batch_size: int = 256
-    epochs: int = 50
+    epochs: int = 100
