c1a480ab44f30649c67c895ffa86bf9bb917b082
tlee08 <timothy.e.lee@sydney.edu.au>
2025-02-14 15:49:23 +1100

    abcd

--- behavysis/scripts/init.py
+++ behavysis/scripts/init.py
@@ -1,7 +1,7 @@
 import os
+import subprocess
 
 from behavysis.utils.misc_utils import get_module_dir
-from behavysis.utils.subproc_utils import run_subproc_console
 
 
 def main() -> None:
@@ -13,9 +13,9 @@ def main() -> None:
     behavysis_dir = get_module_dir("behavysis")
     templates_dir = os.path.join(behavysis_dir, "templates")
     # Installing DEEPLABCUT env
-    run_subproc_console(["cd", templates_dir, "&&" "conda", "env", "create", "-f", "DEEPLABCUT.yaml"])
+    subprocess.call(["cd", templates_dir, "&&" "conda", "env", "create", "-f", "DEEPLABCUT.yaml"])
     # Installing simba env
-    run_subproc_console(["cd", templates_dir, "&&", "conda", "env", "create", "-f", "simba_env.yaml"])
+    subprocess.call(["cd", templates_dir, "&&", "conda", "env", "create", "-f", "simba_env.yaml"])
 
 
 if __name__ == "__main__":
