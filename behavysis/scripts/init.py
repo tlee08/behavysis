c19b0d5d8f6a1dfa24b76c0fe4d78e60558a1ce0
tlee08 <timothyeelim.lee@gmail.com>
2025-03-07 00:57:39 +1100

    abcd

--- behavysis/scripts/init.py
+++ behavysis/scripts/init.py
@@ -1,7 +1,7 @@
 import os
+import subprocess
 
 from behavysis.utils.misc_utils import get_module_dir
-from behavysis.utils.subproc_utils import run_subproc_simple
 
 
 def main() -> None:
@@ -19,7 +19,7 @@ def main() -> None:
         f"conda env create -f {os.path.join(templates_dir, 'DEEPLABCUT.yaml')}",
         f"conda env create -f {os.path.join(templates_dir, 'simba.yaml')}",
     ]:
-        run_subproc_simple(cmd_str)
+        subprocess.run(cmd_str, shell=True, check=True)
 
 
 if __name__ == "__main__":
