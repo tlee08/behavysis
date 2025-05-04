a7f73fcf0aad0295078da4a85d80538e8c0444ce
tlee08 <timothy.e.lee@sydney.edu.au>
2025-05-04 19:30:55 +1000

    changed subproc method and updates pyproject

--- behavysis/processes/extract_features.py
+++ behavysis/processes/extract_features.py
@@ -16,7 +16,7 @@ from behavysis.utils.io_utils import get_name, silent_remove
 from behavysis.utils.logging_utils import get_io_obj_content, init_logger_io_obj
 from behavysis.utils.misc_utils import enum2list
 from behavysis.utils.multiproc_utils import get_cpid
-from behavysis.utils.subproc_utils import run_subproc_logger
+from behavysis.utils.subproc_utils import run_subproc_console
 from behavysis.utils.template_utils import save_template
 
 # Order of bodyparts is from
@@ -180,9 +180,9 @@ def run_simba_subproc(
         "python",
         script_fp,
     ]
-    run_subproc_logger(cmd, logger)
+    # run_subproc_logger(cmd, logger)
+    run_subproc_console(cmd)
     silent_remove(script_fp)
-    logger.info("Ran SimBA feature extraction script.")
 
 
 # TODO: mode/integrate with base_torch_model
