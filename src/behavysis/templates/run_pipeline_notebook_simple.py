ba4bd0c37c1863bf5a573352760f93b933db58c1
tlee08 <timothyeelim.lee@gmail.com>
2026-06-30 20:16:39 +1000

    make simple marimo script for run_pipeline

---
+++ src/behavysis/templates/run_pipeline_notebook_simple.py
@@ -0,0 +1,160 @@
+import marimo
+
+__generated_with = "0.23.10"
+app = marimo.App(width="full")
+
+with app.setup:
+    from collections.abc import Callable
+    from pathlib import Path
+
+    import marimo as mo
+
+    from behavysis import Project
+    from behavysis.constants import DEFAULT_CONFIG_FP
+    from behavysis.funcs import (
+        distance,
+        dur_frames_from_likelihood,
+        in_roi,
+        interpolate,
+        px_per_mm,
+        speed,
+        start_frame_from_likelihood,
+        start_stop_trim,
+        stop_frame_from_dur,
+    )
+    from behavysis.models import ExperimentConfig, get_default_config
+    from behavysis.utils.template_utils import render_template
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Behavysis Pipeline Runner
+    """)
+    return
+
+
+@app.cell
+def _():
+    overwrite = False
+    proj_dir = Path.cwd()
+    names_ls = [i.name for i in (proj_dir / "1_raw_videos").iterdir()]
+    config_fp = proj_dir / "default_config.json"
+    return config_fp, names_ls, overwrite, proj_dir
+
+
+@app.cell
+def _(names_ls, nprocs, proj_dir):
+    proj = Project(proj_dir)
+    proj.nprocs = nprocs.value
+    proj.import_experiments(names_ls)
+    return (proj,)
+
+
+@app.cell
+def _(config_fp, proj):
+    proj.update_config(
+        default_config_fp=config_fp,
+        overwrite="user",
+    )
+    return
+
+
+@app.cell
+def _(overwrite, proj):
+    proj.format_video(
+        overwrite=overwrite,
+    )
+    return
+
+
+@app.cell
+def _(overwrite, proj):
+    proj.run_dlc(
+        gputouse=None,
+        overwrite=overwrite,
+    )
+    return
+
+
+@app.cell
+def _(proj):
+    proj.calculate_parameters(
+        funcs=(
+            start_frame_from_likelihood,
+            stop_frame_from_dur,
+            dur_frames_from_likelihood,
+            px_per_mm,
+        ),
+    )
+    return
+
+
+@app.cell
+def _(overwrite, proj):
+    proj.preprocess(
+        funcs=(
+            start_stop_trim,
+            interpolate,
+        ),
+        overwrite=overwrite,
+    )
+    return
+
+
+@app.cell
+def _(proj):
+    proj.analyse(
+        funcs=(
+            in_roi,
+            speed,
+            distance,
+        ),
+    )
+
+    proj.combine_analysis()
+    proj.collate_analysis()
+    return
+
+
+@app.cell
+def _(overwrite, proj):
+    proj.extract_features(
+        overwrite=overwrite,
+    )
+    return
+
+
+@app.cell
+def _(overwrite, proj):
+    proj.classify_behaviour(overwrite=overwrite.value)
+    proj.export_behaviour(overwrite=overwrite.value)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Manually check labels
+
+    Run behavysis-viewer-app
+    """)
+    return
+
+
+@app.cell
+def _(proj):
+    proj.analyse_behaviour()
+
+    proj.combine_analysis()
+    proj.collate_analysis()
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
